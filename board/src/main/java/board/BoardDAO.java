524863d9c8d5932d8a3ef0d856335c63cb4e94bc
9u4a <99junsung@gmail.com>
2022-05-04 22:42:57 +0900

    Implement AllArticles

--- board/src/main/java/board/BoardDAO.java
+++ board/src/main/java/board/BoardDAO.java
@@ -1,7 +1,10 @@
 package board;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,19 +29,64 @@ public class BoardDAO {
 		}
 	}
 	
-	public List CreateMember() {
+	public List AllArticles(Map pagingMap) {
+		List articlesList = new ArrayList();
+		int section = (Integer)pagingMap.get("section");
+		int pageNum = (Integer)pagingMap.get("pageNum");
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select ROWNUM as recNum,"
+						 	+"art_No,"
+						 	+"title,"
+						 	+"art_ID,"
+						 	+"writeDate,"
+						 	+"category,"
+						 	+"from s_board"
+						 	+"ORDER SIBLINGS BY art_No DESC"
+						 +"where recNum between(?-1)*100+(?-1)*10+1 and (?-1)*100+?*10";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, section);
+			pstmt.setInt(2, pageNum);
+			pstmt.setInt(3, section);
+			pstmt.setInt(4, pageNum);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {			
+				ArticleVO article = new ArticleVO();
+				
+				article.setArt_No(rs.getInt("art_No"));
+				article.setArt_ID(rs.getString("art_ID"));
+				article.setTitle(rs.getString("title"));
+				article.setCategory(rs.getString("category"));
+				article.setWriteDate(rs.getDate("writeDate"));
+				
+				articlesList.add(article);
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		
+		return articlesList;
 	}
 	
-	public List ReadMember() {
-		
-	}
-	
-	public List UpdateMember() {
-		
-	}
-	
-	public List DeleteMember() {
-		
-	}
+//	
+//	public List CreateMember() {
+//		
+//	}
+//	
+//	public List ReadMember() {
+//		
+//	}
+//	
+//	public List UpdateMember() {
+//		
+//	}
+//	
+//	public List DeleteMember() {
+//		
+//	}
 }
