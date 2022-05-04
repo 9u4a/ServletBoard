65b0f93ed3e41078741770498c0ec38a2a4fc187
9u4a <99junsung@gmail.com>
2022-05-05 01:13:20 +0900

    Implement MemberDAO

--- board/src/main/java/board/BoardController.java
+++ board/src/main/java/board/BoardController.java
@@ -1,6 +1,8 @@
 package board;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -8,14 +10,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 
 @WebServlet("/board/*")
 public class BoardController extends HttpServlet {
+	private static String ARTICLE_IMAGE = "C:\\board\\image";
 	private static final long serialVersionUID = 1L;
-	MemberVO memberVO;
+	BoardService boardService;
 	ArticleVO articleVO;
+	MemberDAO memberDAO;
 	
 
     public BoardController() {
@@ -24,30 +29,59 @@ public class BoardController extends HttpServlet {
 
 
 	public void init(ServletConfig config) throws ServletException {
-		memberVO = new MemberVO();
+		boardService = new BoardService();
+		memberDAO = new MemberDAO();
 		articleVO = new ArticleVO();
 	}
-
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
+		doHandle(request, response);
 	}
-
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
+		doHandle(request, response);
 	}
-
-
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		String nextPage="";
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		HttpSession session;
+		String action = request.getPathInfo();
 		
-	}
-
-
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		try {
+				//임시 createMember
+			if(action.equals("/sign.do")) {
+				MemberVO memberVO = new MemberVO();
+				memberVO.setUser_ID(request.getParameter("id"));
+				memberVO.setUser_Pw(request.getParameter("pw"));
+				memberVO.setUser_Name(request.getParameter("name"));
+				memberVO.setUser_Email(request.getParameter("email"));
+				memberDAO.createMember(memberVO);
+				request.setAttribute("createMember", "createMember");
+				nextPage = "/board/main.do";
+				
+				//임시 updateMemberForm
+			} else if(action.equals("/updateMemberForm.do")) {
+				String id = request.getParameter("id");
+				MemberVO findMem = memberDAO.findMember(id);
+				request.setAttribute("findMem", findMem);
+				
+				//임시 updateMember
+			} else if(action.equals("/updateMember.do")) {
+				MemberVO memberVO = new MemberVO();
+				memberVO.setUser_ID(request.getParameter("id"));
+				memberVO.setUser_Pw(request.getParameter("pw"));
+				memberVO.setUser_Name(request.getParameter("name"));
+				memberVO.setUser_Email(request.getParameter("email"));
+				memberDAO.createMember(memberVO);
+				request.setAttribute("createMember", "createMember");
+				nextPage = "/board/main.do";
+				
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ board/src/main/java/board/MemberDAO.java
@@ -0,0 +1,100 @@
+package board;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	
+	private DataSource dataFactory;
+	Connection conn;
+	PreparedStatement pstmt;
+	
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public MemberVO findMember(String findID) {
+		MemberVO findMem = null;
+		try {
+			conn = dataFactory.getConnection();
+			String query = "SELECT * FROM S_USER WHERE user_ID=?";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, findID);
+			ResultSet rs = pstmt.executeQuery();
+			String id = rs.getString("user_ID");
+			String pwd = rs.getString("user_Pw");
+			String name = rs.getString("user_Name");
+			String email = rs.getString("user_Email");
+			findMem = new MemberVO(id, pwd, name, email);
+			pstmt.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return findMem;
+	}
+	
+	public void createMember(MemberVO memberVO) {
+		try {
+			conn = dataFactory.getConnection();
+			String query = "INSERT INTO S_USER (user_ID, user_Pw, user_Name, user_Email)"
+					+ " VALUES (?, ?, ?, ?)";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberVO.getUser_ID());
+			pstmt.setString(2, memberVO.getUser_Pw());
+			pstmt.setString(3, memberVO.getUser_Name());
+			pstmt.setString(4, memberVO.getUser_Email());
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void updateMember(MemberVO memberVO) {
+		try {
+			conn = dataFactory.getConnection();
+			String query = "UPDATE S_USER SET user_Pw=?,user_Name=?,user_Email=?  WHERE user_ID=?";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberVO.getUser_Pw());
+			pstmt.setString(2, memberVO.getUser_Name());
+			pstmt.setString(3, memberVO.getUser_Email());
+			pstmt.setString(4, memberVO.getUser_ID());
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void deleteMember(MemberVO memberVO) {
+		try {
+			conn = dataFactory.getConnection();
+			String query = "DELETE FROM S_USER WHERE user_ID=?";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1,memberVO.getUser_ID());
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
